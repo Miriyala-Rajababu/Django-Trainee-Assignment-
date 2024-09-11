5b8846321ea6738023b8116afd74f628bc3e1d17
raja <rajababumiriyala@gmail.com>
2024-09-11 21:07:22 +0530

    add questions and answers

--- Custom_class.py
+++ Custom_class.py
@@ -1,3 +1,15 @@
+Topic: Custom Classes in Python
+
+Description: You are tasked with creating a Rectangle class with the following requirements:
+
+An instance of the Rectangle class requires length:int and width:int to be initialized.
+We can iterate over an instance of the Rectangle class 
+When an instance of the Rectangle class is iterated over, we first get its length in the 
+format: {'length': <VALUE_OF_LENGTH>} followed by the width {width: <VALUE_OF_WIDTH>}
+
+
+Answer:
+
 class Rectangle:
     def __init__(self, length:int, width:int):
         self.length = length
